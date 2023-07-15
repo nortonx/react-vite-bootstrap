342d13a6ca9359620580ed8cda3af4fb19569bde
Norton Almeida <norton.almeida@gmail.com>
2023-07-15 11:03:48 -0300

    Added unit test and snapshot tests to project.

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../scss/Home.scss";
 const Home = () => {
   return(
-    <div className="container home">Home</div>
+    <div className="container home" data-testid="home">Home</div>
   )
 }
 
